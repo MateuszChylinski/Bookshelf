1088ce54e18c9de30e915277c1906a356a101ec9
Mateusz Chyliński <mateusz.chylinski97@gmail.com>
2026-08-29 19:28:56 +0200

    added request failure explanation that is user friendly, along with http code, and exception status text. The error advice is catching the 4xx/5xx family codes

--- src/main/java/com/example/bookshelf/exception/GlobalControllerAdvice.java
+++ src/main/java/com/example/bookshelf/exception/GlobalControllerAdvice.java
@@ -3,19 +3,38 @@ package com.example.bookshelf.exception;
 import com.example.bookshelf.model.rest.UserQuery;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.client.HttpStatusCodeException;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(HttpStatusCodeException.class)
-    public String handleHttpClientError(HttpStatusCodeException exception, Model model, HttpServletResponse response) {
+    public String handleHttpError(HttpStatusCodeException exception, Model model, HttpServletResponse response) {
+        Map<Integer, String> errors = new HashMap<>();
+
+        if (exception.getStatusCode().is4xxClientError()) {
+            errors.put(exception.getStatusCode().value(),
+                    "The request could not be processed, because of the client side error. (" + exception.getStatusText() + ")");
+
+        } else if (exception.getStatusCode().is5xxServerError()) {
+            errors.put(exception.getStatusCode().value(),
+                    "The request could not be processed, because of the server side error. (" + exception.getStatusText() + ")");
+        } else {
+            errors.put(exception.getStatusCode().value(),
+                    "The request could not be processed, because of the unknown error. (" + exception.getStatusText() + ")");
+        }
+
         response.setStatus(exception.getStatusCode().value());
-        model.addAttribute("globalExceptionHandlerMessage", exception.getMessage());
+        model.addAttribute("globalAdviceRequestFailed", errors);
         return "error";
     }
 
